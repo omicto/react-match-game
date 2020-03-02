80d95b2ec60412ea57d9049227ac0e35be3a6bb7
omicto <tevas.dlr@gmail.com>
2020-03-02 11:19:11 -0700

    Updates

---
+++ precache-manifest.386b316f4dc668d01bca7fd75ab9500f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feebed14ba956623a1e55ee6c6dc7ed3",
+    "url": "/react-match-game/index.html"
+  },
+  {
+    "revision": "9b2f57f92bb55879ca6e",
+    "url": "/react-match-game/static/css/main.ee006e79.chunk.css"
+  },
+  {
+    "revision": "518b052b1ebbaa0d3469",
+    "url": "/react-match-game/static/js/2.149e1e5b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-match-game/static/js/2.149e1e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2f57f92bb55879ca6e",
+    "url": "/react-match-game/static/js/main.2b94ccf9.chunk.js"
+  },
+  {
+    "revision": "851ca5a1552976c2126b",
+    "url": "/react-match-game/static/js/runtime-main.e223f5a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-match-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
